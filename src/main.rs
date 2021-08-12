93c201db4a6d5fccc0a3b67b28ab3565564cad8d
scepter914 <st14.828soccer@gmail.com>
2021-08-13 05:42:54 +0900

    add benchmark

--- src/main.rs
+++ src/main.rs
@@ -24,12 +24,32 @@ impl Benchmark {
     }
 }
 
+fn rgb_to_gray(rgb_image: &image::RgbImage) -> image::GrayImage {
+    let width = rgb_image.width();
+    let height = rgb_image.height();
+    let mut gray_image = image::GrayImage::new(width, height);
+    // gray scale
+    for i in 0..width {
+        for j in 0..height {
+            let pixel = rgb_image.get_pixel(i, j);
+            let gray_pixel = [((pixel[0] as f32 * 0.2126) as u32
+                + (pixel[1] as f32 * 0.7152) as u32
+                + (pixel[2] as f32 * 0.0722) as u32) as u8; 1];
+            gray_image.put_pixel(i, j, image::Luma(gray_pixel));
+        }
+    }
+    return gray_image;
+}
+
 fn main() {
     let device = "/dev/video0";
     let mut camera = rscam::new(device).unwrap();
     let width = 1920;
     let height = 1080;
     let fps = 50;
+    // let width = 640;
+    // let height = 360;
+    // let fps = 330;
     camera
         .start(&rscam::Config {
             interval: (1, fps),
@@ -43,18 +63,8 @@ fn main() {
     for i in 0..100 {
         let frame = camera.capture().unwrap();
         let rgb_image = image::RgbImage::from_vec(width, height, (&frame[..]).to_vec()).unwrap();
+        let gray_image = rgb_to_gray(&rgb_image);
 
-        let mut gray_image = image::GrayImage::new(width, height);
-        // gray scale
-        for i in 0..width {
-            for j in 0..height {
-                let pixel = rgb_image.get_pixel(i, j);
-                let gray_pixel = [((pixel[0] as f32 * 0.2126) as u32
-                    + (pixel[1] as f32 * 0.7152) as u32
-                    + (pixel[2] as f32 * 0.0722) as u32) as u8; 1];
-                gray_image.put_pixel(i, j, image::Luma(gray_pixel));
-            }
-        }
         let otsu_level = imageproc::contrast::otsu_level(&gray_image);
         let binarized_image = imageproc::contrast::threshold(&gray_image, otsu_level);
 
@@ -84,4 +94,15 @@ fn main() {
     let rgb_image_vec = image::RgbImage::from_vec(width, height, (&frame[..]).to_vec()).unwrap();
     bench.print_bench_time();
     rgb_image_vec.save("data/from_vec.png").unwrap();
+
+    println!("rgb to gray");
+    let bench = Benchmark::set_start_time();
+    let gray_image = rgb_to_gray(&rgb_image_vec);
+    bench.print_bench_time();
+
+    println!("otsu binarize");
+    let bench = Benchmark::set_start_time();
+    let otsu_level = imageproc::contrast::otsu_level(&gray_image);
+    let binarized_image = imageproc::contrast::threshold(&gray_image, otsu_level);
+    bench.print_bench_time();
 }
